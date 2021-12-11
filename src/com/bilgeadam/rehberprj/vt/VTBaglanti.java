436053abf163309fdc10ac5a912423e954e6f998
Irem Gunay <iremguunay@gmail.com>
2021-12-11 13:25:51 +0300

    Rehber projesi oluşturuldu

---
+++ src/com/bilgeadam/rehberprj/vt/VTBaglanti.java
@@ -0,0 +1,24 @@
+package com.bilgeadam.rehberprj.vt;
+
+import java.sql.*;
+
+public class VTBaglanti {
+
+    //Static - çünkü nesne oluşturmadan ulaşmak istiyorum
+    public static Connection baglantiGetir() throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.postgresql.Driver");
+
+        Connection conn =
+                DriverManager.getConnection("jdbc:postgresql://localhost:5432/rehber", "postgres", "postgres");
+
+        return conn;
+    }
+
+    public static void baglantiKapat(Connection conn) throws SQLException {
+
+            conn.close();
+
+    }
+
+}
